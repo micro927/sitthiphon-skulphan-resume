38fc07cb59351fda2cc98677c1c290de313c1c03
micro927 <nsbelieve-15@hotmail.com>
2022-11-08 17:17:29 +0700

    done: info API

--- pages/api/information.js
+++ pages/api/information.js
@@ -1,4 +1,8 @@
-export default function getInformation(req, res) {
-    const me = 50000000000
-    res.json({ me })
+import path from 'path';
+import { promises as fs } from 'fs';
+
+export default async function handler(req, res) {
+    const jsonDirectory = path.join(process.cwd(), 'json');
+    const fileContents = JSON.parse(await fs.readFile(jsonDirectory + '/information.json') ?? {})
+    res.status(200).json(fileContents);
 }
